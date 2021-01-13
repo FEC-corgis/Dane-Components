16e21d11c7fa86abdaacadc311f05b03bd04879d
Dane Murphy <danem7@gmail.com>
2021-01-13 15:10:55 -0800

    main photo cropped to correct dimensions

--- frontend/src/components/photos/MainPhoto.jsx
+++ frontend/src/components/photos/MainPhoto.jsx
@@ -2,21 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 
 const MainPhoto = (props) => {
-  return <Container>{<Main src={props.link} alt={'house'} />}</Container>;
+  return (
+    <Container>
+      <Main src={props.link} alt={'house'} />
+    </Container>
+  );
 };
 
 const Container = styled.div`
-  height: 475px;
-  width: 575px;
+  height: 504px;
+  width: 564px;
+  border-top-left-radius: 1rem;
+  border-bottom-left-radius: 1rem;
+  overflow: hidden;
 `;
 
 const Main = styled.img`
-  border-top-left-radius: 1rem;
-  border-bottom-left-radius: 1rem;
   height: 100%;
-  width: 100%;
-  object-fit: cover;
+  margin-left: -95px;
 `;
-//transform: scale(1.2);
 
 export default MainPhoto;
