482371dcbde1699f4f835c3b3e6ec547daed31e8
Dane Murphy <danem7@gmail.com>
2021-01-18 18:59:25 -0800

    switched api call

--- frontend/src/redux/slices/photos/helpers.js
+++ frontend/src/redux/slices/photos/helpers.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export async function getPhotos() {
-  const res = await axios.get('http://localhost:5000/api/photos/10');
+  const res = await axios.get('http://localhost:5000/api/photos/1');
   return res.data;
 }
