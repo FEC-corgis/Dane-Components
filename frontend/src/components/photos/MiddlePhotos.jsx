e18d78c86aa4c1d19ad05ef8ab3b28f9ab25a520
Dane Murphy <danem7@gmail.com>
2021-01-21 19:11:25 -0800

    small cleanup

--- frontend/src/components/photos/MiddlePhotos.jsx
+++ frontend/src/components/photos/MiddlePhotos.jsx
@@ -3,23 +3,21 @@ import MiddleImg from './styled-components/MiddleImg';
 import ContainerTop from './styled-components/ContainerTop';
 import ContainerBottom from './styled-components/ContainerBottom';
 
-const MiddlePhotos = (props) => {
-  return (
-    <React.Fragment>
-      <ContainerTop
-        className={'photo-container'}
-        onClick={() => props.handleClick(props.photo1.id)}
-      >
-        <MiddleImg src={props.photo1.link} alt={'house'} />
-      </ContainerTop>
-      <ContainerBottom
-        className={'photo-container'}
-        onClick={() => props.handleClick(props.photo2.id)}
-      >
-        <MiddleImg src={props.photo2.link} alt={'house'} />
-      </ContainerBottom>
-    </React.Fragment>
-  );
-};
+const MiddlePhotos = (props) => (
+	<React.Fragment>
+		<ContainerTop
+			className={'photo-container'}
+			onClick={() => props.handleClick(props.photo1.id)}
+		>
+			<MiddleImg src={props.photo1.link} alt={'house'} />
+		</ContainerTop>
+		<ContainerBottom
+			className={'photo-container'}
+			onClick={() => props.handleClick(props.photo2.id)}
+		>
+			<MiddleImg src={props.photo2.link} alt={'house'} />
+		</ContainerBottom>
+	</React.Fragment>
+);
 
 export default MiddlePhotos;
