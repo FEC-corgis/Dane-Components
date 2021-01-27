72d1f4def13c094dacb798cbdfd49dc2e463765b
Dane Murphy <danem7@gmail.com>
2021-01-27 11:13:34 -0800

    removed conditional route to get data

--- frontend/src/redux/slices/photos/helpers.js
+++ frontend/src/redux/slices/photos/helpers.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
 export async function getPhotos(id) {
-    const route =
-        process.env.NODE_ENV !== 'production'
-            ? `http://localhost:5000/api/photos/${id}`
-            : `/api/headerService/photos/${id}`;
-
-    const { data } = await axios.get(route);
+    const { data } = await axios.get(`/api/headerService/photos/${id}`);
     return data;
 }
