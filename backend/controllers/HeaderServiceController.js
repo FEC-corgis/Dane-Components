4e6f5bdf4f63272599e2e66e7f51904cc85fa060
Dane Murphy <danem7@gmail.com>
2021-01-27 23:57:15 -0800

    cache implemented

--- backend/controllers/HeaderServiceController.js
+++ backend/controllers/HeaderServiceController.js
@@ -3,12 +3,13 @@ const client = require('redis').createClient();
 const HostedByRepository = require('../repositories/HostedByRepo');
 
 router.get('/:propertyId', async (req, res) => {
-    const repo = new HostedByRepository(req.params.propertyId);
+    const { propertyId } = req.params;
+    const repo = new HostedByRepository(propertyId);
     try {
         const response = await repo.getData();
         const cacheData = JSON.stringify(response);
 
-        client.setex(req.params.propertyId, 3600, cacheData);
+        client.setex(`header${propertyId}`, 3600, cacheData);
         return res.status(200).json(response);
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error' });
