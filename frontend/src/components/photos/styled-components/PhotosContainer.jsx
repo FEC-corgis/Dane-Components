9fb7f6a01f4d50d7e4909c9f6112f8b59973f13c
Dane Murphy <danem7@gmail.com>
2021-01-31 15:58:02 -0800

    commit

--- frontend/src/components/photos/styled-components/PhotosContainer.jsx
+++ frontend/src/components/photos/styled-components/PhotosContainer.jsx
@@ -3,6 +3,5 @@ import styled from 'styled-components';
 export default styled.div`
     max-width: 1128px;
     margin: 0 auto;
-    border-bottom: 1px solid rgb(221, 221, 221);
     padding-bottom: 48px;
 `;
