2843896dd0e3f11d1739111cf666f751e083fdb4
Dane Murphy <danem7@gmail.com>
2021-02-03 21:36:46 -0800

    modal background bug addressed

--- frontend/src/components/modal/share/Links.js
+++ frontend/src/components/modal/share/Links.js
@@ -3,42 +3,42 @@ import { IoLogoWhatsapp } from 'react-icons/io';
 import { ImEmbed2 } from 'react-icons/im';
 import { AiOutlineTwitter, AiFillMessage } from 'react-icons/ai';
 import {
-  FaFacebookF,
-  FaClipboardList,
-  FaFacebookMessenger,
+    FaFacebookF,
+    FaClipboardList,
+    FaFacebookMessenger,
 } from 'react-icons/fa';
 
 export default [
-  {
-    name: 'Facebook',
-    icon: FaFacebookF,
-  },
-  {
-    name: 'Twitter',
-    icon: AiOutlineTwitter,
-  },
-  {
-    name: 'Copy Link',
-    icon: FaClipboardList,
-  },
-  {
-    name: 'Email',
-    icon: BsEnvelopeFill,
-  },
-  {
-    name: 'SMS',
-    icon: AiFillMessage,
-  },
-  {
-    name: 'Messenger',
-    icon: FaFacebookMessenger,
-  },
-  {
-    name: 'WhatsApp',
-    icon: IoLogoWhatsapp,
-  },
-  {
-    name: 'Embed',
-    icon: ImEmbed2,
-  },
+    {
+        name: 'Facebook',
+        icon: FaFacebookF,
+    },
+    {
+        name: 'Twitter',
+        icon: AiOutlineTwitter,
+    },
+    {
+        name: 'Copy Link',
+        icon: FaClipboardList,
+    },
+    {
+        name: 'Email',
+        icon: BsEnvelopeFill,
+    },
+    {
+        name: 'SMS',
+        icon: AiFillMessage,
+    },
+    {
+        name: 'Messenger',
+        icon: FaFacebookMessenger,
+    },
+    {
+        name: 'WhatsApp',
+        icon: IoLogoWhatsapp,
+    },
+    {
+        name: 'Embed',
+        icon: ImEmbed2,
+    },
 ];
