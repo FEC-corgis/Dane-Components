50f97791b5c1f03fc623052fc2cb57a999fb71ae
Dane Murphy <danem7@gmail.com>
2021-02-05 12:11:58 -0800

    test for more places endpoint working

--- backend/controllers/__tests__/HeaderService.test.js
+++ backend/controllers/__tests__/HeaderService.test.js
@@ -1,6 +1,7 @@
-const app = require('../server');
+const app = require('../../server');
 const request = require('supertest');
-const { server } = require('../../mock/server/backend');
+const { server } = require('../../../mock/server/backend');
+const { mockSuperhostData } = require('../../../mock/mockData/backend');
 
 beforeAll(() => server.listen());
 
@@ -14,6 +15,12 @@ test('should query the photos of the property with ID 1', async () => {
     expect(res.status).toEqual(200);
     expect(res.body.photos[0].Property.id).toEqual(1);
     expect(res.body.photos.length).toEqual(10);
+    expect(res.body.isSuperhost).toBe(mockSuperhostData[0]);
+    expect(res.body.location).toEqual({
+        city: 'South Lake Tahoe',
+        state: 'California',
+        country: 'United States',
+    });
 });
 
 test('should query the photos of the property with ID 80', async () => {
@@ -22,4 +29,10 @@ test('should query the photos of the property with ID 80', async () => {
     expect(res.status).toEqual(200);
     expect(res.body.photos[0].Property.id).toEqual(80);
     expect(res.body.photos.length).toEqual(5);
+    expect(res.body.isSuperhost).toBe(mockSuperhostData[1]);
+    expect(res.body.location).toEqual({
+        city: 'South Lake Tahoe',
+        state: 'California',
+        country: 'United States',
+    });
 });
--- mock/handlers/backend.js
+++ mock/handlers/backend.js
@@ -1,17 +1,17 @@
 const { rest } = require('msw');
-const { mockHostedbyData, mockReviewsData } = require('../mockData/backend');
+const { mockSuperhostData, mockReviewsData } = require('../mockData/backend');
 
 module.exports.handlers = [
     rest.get(
         'http://localhost:5002/api/hostedbyService/superhost/1',
         (_req, res, ctx) => {
-            return res(ctx.json(mockHostedbyData[0]));
+            return res(ctx.json(mockSuperhostData[0]));
         }
     ),
     rest.get(
         'http://localhost:5002/api/hostedbyService/superhost/80',
         (_req, res, ctx) => {
-            return res(ctx.json(mockHostedbyData[1]));
+            return res(ctx.json(mockSuperhostData[1]));
         }
     ),
     rest.get('http://localhost:1984/reviews/header/1', (_req, res, ctx) => {
