2cdce581a01caacac8f190b5951c990fe5a576f8
Dane Murphy <danem7@gmail.com>
2021-02-05 11:36:39 -0800

    figured out how to mock external apis

--- backend/mock/handlers.js
+++ backend/mock/handlers.js
@@ -2,8 +2,14 @@ const { rest } = require('msw');
 const { mockData } = require('./mockData');
 
 module.exports.handlers = [
-    rest.get('/api/headerService/1', (_req, res, ctx) => {
-        return res(ctx.json(mockData[0]));
+    rest.get(
+        'http://localhost:5002/api/hostedbyService/superhost/1',
+        (_req, res, ctx) => {
+            return res(ctx.json(mockData[2]));
+        }
+    ),
+    rest.get('http://localhost:1984/reviews/header/1', (_req, res, ctx) => {
+        return res(ctx.json(mockData[3]));
     }),
     rest.get('/api/headerService/80', (_req, res, ctx) => {
         return res(ctx.json(mockData[1]));
